cd461427d40f077cb5ff217ff8dd4c89452a9361
Amlesh-Singh <amlesh5001@gmail.com>
2017-09-29 08:41:14 +0530

    c

---
+++ src/com/app/Amlesh.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Amlesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Jai Mata Di");
+System.out.println("Jai Mata Ho...");
+System.out.println("Jai Mata Kripa");
+System.out.println("Jai Mata mafi");
+System.out.println("Jai Mata Di");
+System.out.println("Jai Mata Di");
+	}
+
+}
